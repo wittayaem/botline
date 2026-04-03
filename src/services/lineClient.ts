ed8d127debd0f2374dded217a67bf2a91dc01ed3
wittayaem <wittayabboyem@hotmail.com>
2026-04-03 17:47:58 +0700

    Delete src directory

--- src/services/lineClient.ts
+++
@@ -1,8 +0,0 @@
-import * as line from '@line/bot-sdk';
-
-const config: line.ClientConfig = {
-  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN!,
-};
-
-export const client = new line.messagingApi.MessagingApiClient(config);
-export const blobClient = new line.messagingApi.MessagingApiBlobClient(config);
--- src/utils/logger.ts
+++
@@ -1,5 +0,0 @@
-import pino from 'pino';
-
-const logger = pino({ level: 'info' }, process.stdout);
-
-export default logger;
